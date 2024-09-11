3d435e8e6c39e77f77e982a89426bc21a5836f6c
yahya0morsy <ym60307@gmail.com>
2024-09-11 10:54:54 +0300

    sexth2-commit

--- src/notes.jsx
+++ src/notes.jsx
@@ -5,7 +5,7 @@ function Notes(){
     const [z ,setz] =useState([])
     const [del ,setd] =useState()
      async function Addnote(){
-            await axios.post('https://note-back-nine.vercel.app/notes/save', {
+            await axios.post('/notes/save', {
               title:document.getElementById("title").value,
               data:document.getElementById("text").value
             }, { withCredentials: true })
@@ -18,9 +18,10 @@ function Notes(){
             })
     }
     function show(){ 
-        axios.get("https://note-back-nine.vercel.app/notes/show", { withCredentials: true }).then(function(res){ setz(res.data) ,console.log(res.data)})
+        axios.get("/notes/show", { withCredentials: true }).then(function(res){ setz(res.data) ,console.log(res.data)})
 
   } 
+  //https://note-back-nine.vercel.app
     function sure(x){
         document.getElementById("sure").textContent = "are you sure you want to delete this?"
         document.getElementById("yes").textContent = "yes" 
@@ -29,7 +30,7 @@ function Notes(){
 
     }
    function deleteNote(){
-        axios.post('https://note-back-nine.vercel.app/notes/delete',{
+        axios.post('/notes/delete',{
             title:del 
           }, { withCredentials: true })
           .then(function (response) {
