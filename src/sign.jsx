522e551eb4b9d602e7d23fedcb6c3595ddb93f9e
yahya0morsy <ym60307@gmail.com>
2024-09-27 20:38:28 +0300

    working107 commit

--- src/sign.jsx
+++ src/sign.jsx
@@ -10,7 +10,7 @@ function Sign(){
   async function submit(){
     await axios.post('https://note-back-mode2-teri.vercel.app/log/auth', {
       Username:document.getElementById("user").value,
-      Password:document.getElementById("pass").value
+      Password:document.getElementById("Mypassword").value
     })
     .then(function (response) {
       if(response.data.Message=="logged in"){navigate('/try', { replace: true })};console.log(response.data);
@@ -26,6 +26,15 @@ function Sign(){
    
   
   }
+  function hider() {
+    
+    const x = document.getElementById("Mypassword");
+    if (x.type == "password"){
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
   function checker(responde){
     if(responde){
       document.getElementById("state").textContent = "wrong username or password";
@@ -39,7 +48,8 @@ function Sign(){
 
         <div className=" bg-gray-500 place-self-center sign flex flex-col place-items-start border-4 border-black">
         <div className="sm:text-lg md:text-xl lg:text-2xl my-2 mx-2">username: <input type="text" className="h-10 w-48 border-2 border-black rounded-md" id="user"></input></div>
-        <div className="sm:text-lg md:text-xl lg:text-2xl my-2 mx-2">password: <input type="text" className="h-10 w-48 border-2 border-black rounded-md" id="pass"></input></div>
+        <div className="sm:text-lg md:text-xl lg:text-2xl my-2 mx-2">password: <input type="password" className="h-10 w-48 border-2 border-black rounded-md" id="Mypassword"></input><input className="size-4 mx-1" type="checkbox" onChange={()=>hider()}/></div>
+        
         <div className="flex flex-row ">
         <button className="w-20 h-8 mx-2 bg-white rounded-md hover:bg-blue-500 hover:shadow-md hover:shadow-black duration-150" onClick={()=>submit()}>submit</button>
         <div id="state" className="">enter your login data</div>
